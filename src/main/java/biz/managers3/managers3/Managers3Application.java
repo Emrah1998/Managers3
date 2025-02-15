7579704b3a454dfc4497843a5991b060bcb11657
Emrah1998 <emrahqarawov240@gmail.com>
2025-02-15 12:41:53 +0400

    first commit

---
+++ src/main/java/biz/managers3/managers3/Managers3Application.java
@@ -0,0 +1,13 @@
+package biz.managers3.managers3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Managers3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Managers3Application.class, args);
+    }
+
+}
---
+++ src/test/java/biz/managers3/managers3/Managers3ApplicationTests.java
@@ -0,0 +1,13 @@
+package biz.managers3.managers3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Managers3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
